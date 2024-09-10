696fddb7d9e25108b657a17e5a145343bdcd4a0f
Anupam Yadav <anupamy571@gmail.com>
2024-09-10 14:11:57 +0530

    API modified and error handled

--- src/pages/Home/AllRestaurants/AllRestaurants.jsx
+++ src/pages/Home/AllRestaurants/AllRestaurants.jsx
@@ -5,7 +5,7 @@ import { v4 as generateId } from "uuid";
 import { IoCloseOutline } from "react-icons/io5";
 import { useDispatch } from "react-redux";
 import { setFilterData } from "../../../utils/slices/filterSlice";
-import NoImgFound from "../../../assets/data.png";
+// import NoImgFound from "../../../assets/data.png";
 
 const AllRestaurants = ({ restaurantData, title }) => {
   console.log(title);
@@ -52,22 +52,23 @@ const AllRestaurants = ({ restaurantData, title }) => {
               className="flex cursor-pointer items-center gap-2 rounded-[18px] border border-gray-300 px-4 py-2 text-gray-800"
             >
               <span className="text-xs font-medium sm:text-sm">
-                {item.title}
+                {item?.title}
               </span>
               {activeBtn === item.title && <IoCloseOutline size={20} />}
             </div>
           ))}
         </div>
       </div>
-      {restaurantData.length === 0 ? (
+      {restaurantData?.length === 0 ? (
         <div className="mx-auto flex w-[85%] items-center justify-center gap-2 py-6">
-          <img className="-mt-20 w-[50%]" src={NoImgFound} alt="" />
+          {/* <img className="-mt-20 w-[50%]" src={NoImgFound} alt="" /> */}
+          no image found
         </div>
       ) : (
         <div className="grid grid-cols-1 justify-center gap-5 sm:grid-cols-2 sm:p-0 md:grid-cols-3 lg:grid-cols-4">
           {restaurantData &&
-            restaurantData.map((item) => {
-              return <RestaurentCard key={item.info.id} item={item} />;
+            restaurantData?.map((item) => {
+              return <RestaurentCard key={item?.info?.id} item={item} />;
             })}
         </div>
       )}
